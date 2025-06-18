2075e7b439b388c9f3dbe0c6ca99f30999159cf2
Carlos Araujo <carlos.araujo.aus@gmail.com>
2025-06-18 12:09:59 +0800

    method handlers added an events on buttons

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
 
@@ -7,21 +6,34 @@ class ContentRating extends Component {
     super();
     this.state = { likes: 0, dislikes: 0 }
   }
+  
+  handleLike = () => {
+    this.setState((prevState) => ({
+      likes: prevState.likes + 1
+    }));
+  }
+  handleDislike = () => {
+    this.setState((prevState) => ({
+      dislikes: prevState.dislikes + 1
+    }));
+  }
+
   render() {
     return (
-     <div className='content-rating'>
-      <p>
-        {/* Add text here */}
-      </p>
-        <div className='raing-buttons'>
-          <button className='like-button'> 
-            Like ({this.state.likes}) 
-          </button>
-          <button className='dislike-button'> 
-            Dislike ({this.state.dislikes}) 
-          </button>
-        </div>
-      </div>     
+      <>
+        <h1>Text Content Rating</h1>
+        <div className='content-rating'>
+          <p>Text</p>
+          <div className='raing-buttons'>
+            <button className='like-button' onClick={this.handleLike}> 
+              Like ({this.state.likes}) 
+            </button>
+            <button className='dislike-button' onClick={this.handleDislike}> 
+              Dislike ({this.state.dislikes}) 
+            </button>
+          </div>
+        </div>     
+      </>
     );
   }
 }
